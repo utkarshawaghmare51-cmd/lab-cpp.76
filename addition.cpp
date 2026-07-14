63fcf14a286111f20c2cef7fadc931ce9fa39948
utkarshawaghmare51-cmd <utkarshawaghmare51@gmail.com>
2026-07-14 18:38:36 +0000

    addition program

---
+++ addition.cpp
@@ -0,0 +1,16 @@
+#include<iostream>
+using namespace std;
+
+int main()
+{
+    int a, b, sum;
+
+    cout<<"enter two numbers: ";
+    cin>> a>>b;
+
+    sum = a+b;
+
+    cout <<"addition ="<<sum;
+
+    return 0;
+}
