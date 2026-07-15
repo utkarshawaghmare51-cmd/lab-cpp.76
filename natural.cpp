35e310ee82992ba139a33d3c4cf4a86c0c22ca57
utkarshawaghmare51-cmd <utkarshawaghmare51@gmail.com>
2026-07-15 11:06:34 +0000

    natural number

---
+++ natural.cpp
@@ -0,0 +1,17 @@
+#include<iostream>
+using namespace std;
+
+int main()
+{
+    int n,i =1;
+
+    cout<< "enetr the value of n :" ;
+    cin>> n;
+
+    while(i <=n)
+    {
+        cout << i << endl;
+        i++;
+    }
+    return 0;
+}
